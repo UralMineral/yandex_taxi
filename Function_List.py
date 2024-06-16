decfca597ae9cdd510469f2ebd6857b4ab85c954
Alikhan Aitbayev <alihanaitbaev2000@gmail.com>
2024-06-16 17:50:32 +0500

    ALMAZ COMMIT

--- Function_List.py
+++ Function_List.py
@@ -19,7 +19,7 @@
                           
         error_estimator(models, fig_size)
         '''
-
+# ALMAZ
 def mean_absolute_percentage_error(y_true, y_pred):
     return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
